59b38447b0ff8b4130f5251d8ba3b23968e9fb60
Venkatesh Ravi <kumarvenkatesh76@gmail.com>
2025-03-19 06:28:39 +0100

    added service

--- src/app/services/regions.service.spec.ts
+++ src/app/services/regions.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RegionsService } from './regions.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('RegionsService', () => {
   let service: RegionsService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    });
     service = TestBed.inject(RegionsService);
   });
 
-  it('should be created', () => {
+  it('should create service', () => {
     expect(service).toBeTruthy();
   });
 });
